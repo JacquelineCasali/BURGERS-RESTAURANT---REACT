4c0addd79741214626d98e33e17386819a5cba77
Jacqueline <jcasalia@yahoo.com.br>
2024-07-17 14:22:14 -0300

    seta abre e fecha

--- frontEnd/src/components/Card/CardList.jsx
+++ frontEnd/src/components/Card/CardList.jsx
@@ -3,7 +3,7 @@ import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 
 import { BsSearch } from "react-icons/bs";
-import { FiChevronUp } from "react-icons/fi";
+import { FiChevronUp, FiChevronDown } from "react-icons/fi";
 import Menu from "../Menu/Menu";
 import data from "../../data/db.json";
 import Banner from "../Banner/Banner";
@@ -23,10 +23,12 @@ const CardList = ({
   onLerDrinks,
   onLerDessert,
 }) => {
- 
-  const{cartItems,setCartItems} =useContext(AppContext);
+  const { cartItems, setCartItems } = useContext(AppContext);
   const [busca, setBusca] = useState("");
-
+  // seta
+  const [isSeta, setIsSeta] = useState(true);
+  const [isSetaDrinks, setIsSetaDrinks] = useState(true);
+  const [isSetaDessert, setIsSetaDessert] = useState(true);
   //   // filtro
   // //convertendo em letra minuscula
   const searchLowerCase = busca.toLowerCase();
@@ -40,21 +42,48 @@ const CardList = ({
     item.name.toLowerCase().includes(searchLowerCase)
   );
 
-  const handleChange = (item, d) =>{
-		let ind = -1;
-		cartItems.forEach((data, index)=>{
-			if (data.id === item.id)
-				ind = index;
-		});
-		const tempArr = cartItems;
-		tempArr[ind].visible += d;
-		
-		if (tempArr[ind].visible === 0)
-			tempArr[ind].visible = 1;
-		setCartItems([...tempArr])
-	}
+  function openSeta() {
+    setIsSeta(false);
 
+  }
+
+  function closeSeta() {
+    setIsSeta(true);
  
+
+  }
+  function openSetaDrink() {
+    setIsSetaDrinks(false);
+
+  }
+
+  function closeSetaDrink() {
+  
+    setIsSetaDrinks(true)
+
+  }
+
+  function openSetaDessert() {
+    setIsSetaDessert(false);
+
+  }
+
+  function closeSetaDessert() {
+  
+    setIsSetaDessert(true)
+
+  }
+  const handleChange = (item, d) => {
+    let ind = -1;
+    cartItems.forEach((data, index) => {
+      if (data.id === item.id) ind = index;
+    });
+    const tempArr = cartItems;
+    tempArr[ind].visible += d;
+
+    if (tempArr[ind].visible === 0) tempArr[ind].visible = 1;
+    setCartItems([...tempArr]);
+  };
 
   return (
     <>
@@ -79,38 +108,95 @@ const CardList = ({
 
             <div className="menu-sections">
               <h1 id="burgers">{data.sections[0].name}</h1>
-              <FiChevronUp size={26} cursor={"pointer"} color="#4F372F" />
+              {isSeta ? (
+                <FiChevronUp
+                  onClick={openSeta}
+                  size={26}
+                  cursor={"pointer"}
+                  color="#4F372F"
+                />
+              ) : (
+                <FiChevronDown
+                  onClick={closeSeta}
+                  size={26}
+                  cursor={"pointer"}
+                  color="#4F372F"
+                />
+              )}
             </div>
-
-            {teste.map((product) => (
-              <Link key={product.id} onClick={() => onLer(product.id)}>
-                <CardDetalhe data={product} />
-              </Link>
-            ))}
+            {isSeta && (
+              <>
+                {teste.map((product) => (
+                  <Link key={product.id} onClick={() => onLer(product.id)}>
+                    <CardDetalhe data={product} />
+                  </Link>
+                ))}
+              </>
+            )}
             <div className="menu-sections">
               <h1 id="drinks">{data.sections[1].name}</h1>
-              <FiChevronUp size={26} cursor={"pointer"} color="#4F372F" />
-            </div>
 
+              {isSetaDrinks ? (
+                <FiChevronUp
+                  onClick={openSetaDrink}
+                  size={26}
+                  cursor={"pointer"}
+                  color="#4F372F"
+                />
+              ) : (
+                <FiChevronDown
+                  onClick={closeSetaDrink}
+                  size={26}
+                  cursor={"pointer"}
+                  color="#4F372F"
+                />
+              )}
+              {/* <FiChevronUp size={26} cursor={"pointer"} color="#4F372F" /> */}
+            </div>
+            {isSetaDrinks && (
+              <>
             {drinks.map((product) => (
               <Link key={product.id} onClick={() => onLerDrinks(product.id)}>
                 <CardDetalheDrink data={product} />
               </Link>
             ))}
+            </>
+          )}
             <div className="menu-sections">
               <h1 id="desserts">{data.sections[2].name}</h1>
-              <FiChevronUp size={26} cursor={"pointer"} color="#4F372F" />
+            
+              {isSetaDessert ? (
+                <FiChevronUp
+                  onClick={openSetaDessert}
+                  size={26}
+                  cursor={"pointer"}
+                  color="#4F372F"
+                />
+              ) : (
+                <FiChevronDown
+                  onClick={closeSetaDessert}
+                  size={26}
+                  cursor={"pointer"}
+                  color="#4F372F"
+                />
+              )}
+
             </div>
-            {desserts.map((product) => (
+            {isSetaDessert && (
+           <>
+           {desserts.map((product) => (
               <Link key={product.id} onClick={() => onLerDessert(product.id)}>
                 <CardDetalhe data={product} />
               </Link>
             ))}
+            </>
+          )}
           </div>
 
-          <Cart cartItems={cartItems}
-           setCartItems={setCartItems} 
-           handleChange={handleChange}
+          <Cart
+            cartItems={cartItems}
+            setCartItems={setCartItems}
+            handleChange={handleChange}
           />
         </div>
       </section>
